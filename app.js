e7dc6d491332ff760cabf09deb94111b949edf8b
Miya Lucas <srlucas3@gmail.com>
2017-07-01 17:02:45 -0700

    it works! yay!

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var allOptions = [];
 //these are meant to represent the three images that will show for each round
 var firstImageEl = document.getElementById('first-image');
 var secondImageEl = document.getElementById('second-image');
@@ -12,6 +12,7 @@ function Image(name, path) {
   this.shown = 0;
   this.clicked = 0;
   this.helpfulId = '';
+  allOptions.push(this);
 }
 
 //something to think about for later
@@ -22,7 +23,8 @@ function Image(name, path) {
 
 var bag = new Image('bag', './assets/bag.jpg');
 var banana = new Image('banana', './assets/banana.jpg');
-var boots = new Image('bathroom', './assets/bathroom.jpg');
+var bathroom = new Image('bathroom', './assets/bathroom.jpg');
+var boots = new Image('boots', './assets/boots.jpg');
 var breakfast = new Image('breakfast', './assets/breakfast.jpg');
 var bubblegum = new Image('bubblegum', './assets/bubblegum.jpg');
 var chair = new Image('chair', './assets/chair.jpg');
@@ -40,7 +42,7 @@ var usb = new Image('usb', './assets/usb.gif');
 var waterCan = new Image('water-can', './assets/water-can.jpg');
 var wineGlass = new Image('wine-glass', './assets/wine-glass.jpg');
 
-var allOptions = [bag, banana, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
+
 
 //then google a random function to pick stuff out of an array
 // Array.prototype.randomizedOption = function() {
@@ -51,22 +53,38 @@ var allOptions = [bag, banana, boots, breakfast, bubblegum, chair, cthulhu, dogD
 
 
 var randomImages = [];
+var tempImages = [];
+
+randomizedOption();
+displayRandomImages();
 
 //below is a function that returns a random number which should correspond to the images in my array
 function randomizedOption() {
   //var numbers = [];
-  while (randomImages.length < 3) {
+  while (tempImages.length < 3) {
       var randomNumber = Math.floor(Math.random() * allOptions.length)
-      if (!randomImages.includes(allOptions[randomNumber])) {
-        randomImages.push(allOptions[randomNumber]);
+      if (!tempImages.includes(allOptions[randomNumber]) && !randomImages.includes(allOptions[randomNumber])) {
+        tempImages.push(allOptions[randomNumber]);
+        // if (tempImages[0] != randomImages[0] && tempImages[0] != randomImages[1] && tempImages[0] != randomImages[2] && tempImages[1] != randomImages[0] && tempImages[1] != randomImages[1] && tempImages[1] != randomImages[2] && tempImages[2] != randomImages[0] && tempImages[2] != randomImages[1] && tempImages[2] != randomImages[2]) {
       }
+    // }
   }
+  randomImages = tempImages;
+  console.log(randomImages);
+  console.log(tempImages);
+  tempImages = [];
 }
 
 function displayRandomImages() {
 firstImageEl.src = randomImages[0].path;
+firstImageEl.name = randomImages[0].name;
+randomImages[0].shown++;
 secondImageEl.src = randomImages[1].path;
+secondImageEl.name = randomImages[1].name;
+randomImages[1].shown++;
 thirdImageEl.src = randomImages[2].path;
+thirdImageEl.name = randomImages[2].name;
+randomImages[2].shown++;
 }
 
 //write a for loop that calls both functions (the randomizedOption() and displayRandomImages())
@@ -84,25 +102,37 @@ var clicked = 0;
 //use event listener to respond each time the user clicks or chooses an option and store the number of clicks as well as percentage of clicks - it will modify what is already in the constructor function
 var formEl = document.getElementsByClassName('form')[0];
 
+
 formEl.addEventListener('click', handleClicked);
+console.log(rounds);
+
 
 function handleClicked(event) {
-
-  // while (rounds < 25) {
+  if (rounds < 25) {
     event.preventDefault();
-    var itemName = event.target;
-    // randomizedOption();
-    // displayRandomImages();
-    // rounds++;
-    // clicked++;
-    // //clicked++
-
-
+    var itemName = event.target.name;
+      for (var i=0; i < allOptions.length; i++) {
+        var currentImage = allOptions[i];
+        if (currentImage.name === itemName) {
+          currentImage.clicked++;
+          console.log(currentImage);
+        }
  console.log(itemName);
   }
-// }
+  randomizedOption();
+  displayRandomImages();
+} else {
+  for (var i=0; i < allOptions.length; i++) {
+    allOptions[i].percentageClicked = allOptions[i].clicked / allOptions[i].shown * 100;
+  }
+  console.log(allOptions);
+}
+  rounds++;
+}
 
-handleClicked(event);
+//display all user choices only after user has made 25 selections
+
+// alert('Here are your choices:')
 
 
 //show random options
@@ -114,6 +144,5 @@ handleClicked(event);
 
 //no need for a for loop, instead do answer++
 
-//display all user choices only after user has made 25 selections
 
 //then write a function that calculates percentages of item chosen when shown

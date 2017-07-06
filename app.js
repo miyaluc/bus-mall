7854e6777fe020d0af93e24a8ef9a6ee5f077de3
Miya Lucas <srlucas3@gmail.com>
2017-07-05 20:13:43 -0700

    added chart to js but receiving a context-related error

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 'use strict';
+
+var chartColors = ['black', 'purple', 'brown', 'green', 'blue', 'red'];
+
 var allOptions = [];
 //these are meant to represent the three images that will show for each round
 var firstImageEl = document.getElementById('first-image');
@@ -59,6 +62,7 @@ randomizedOption();
 displayRandomImages();
 
 //below is a function that returns a random number which should correspond to the images in my array
+//test for duplicates (this can be perfected later)
 function randomizedOption() {
   //var numbers = [];
   while (tempImages.length < 3) {
@@ -72,6 +76,7 @@ function randomizedOption() {
   randomImages = tempImages;
   console.log(randomImages);
   console.log(tempImages);
+  //after an option is chosen, erase everything and run the random function again
   tempImages = [];
 }
 
@@ -91,6 +96,7 @@ randomImages[2].shown++;
 
 var rounds = 0;
 var clicked = 0;
+
 
 // while (rounds < 25) {
 //   randomizedOption();
@@ -123,6 +129,7 @@ function handleClicked(event) {
   displayRandomImages();
 } else {
   for (var i=0; i < allOptions.length; i++) {
+    //then write a function that calculates percentages of item chosen when shown
     allOptions[i].percentageClicked = allOptions[i].clicked / allOptions[i].shown * 100;
   }
   console.log(allOptions);
@@ -130,19 +137,80 @@ function handleClicked(event) {
   rounds++;
 }
 
-//display all user choices only after user has made 25 selections
+// display all user choices only after user has made 25 selections
+// use a prototype function
 
-// alert('Here are your choices:')
-
-
-//show random options
-
-//test for duplicates (this can be perfected later)
+var imageNames = [];
+var imagesClicked= [];
+var imagesShown = [];
 
 
-//after an option is chosen, erase everything and run the random function again
+function getVoteTotals() {
+  allOptions.sort(function(x, y) {
+    return y.clicked - x.clicked;
+  })
+  displayChoices();
+}
 
+var context = document.getElementById('my-chart').getContext('2d');
+
+function displayChoices() {
+  for (var i = 0; i < allOptions.length; i++) {
+    imageNames.push(allOptions[i].name)
+  }
+
+  for (var i = 0; i < allOptions.length; i++) {
+    imagesClicked.push(allOptions[i].clicked)
+  }
+
+  for (var i = 0; i < allOptions.length; i++) {
+    imagesShown.push(allOptions[i].shown)
+  }
+
+ var myChart = new Chart(context, {
+  type: 'bar',
+  data: {
+    labels: imageNames,
+    datasets: [{
+      label: '# of Votes',
+      data: imagesClicked,
+      backgroundColor: chartColors,
+    }],
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+        },
+      }],
+    },
+  },
+});
+}
+  // alert('Here are your choices.');
+  //
+  // document.write(bag.clicked 'votes for the Star Wars bag.');
+  // document.write(banana.clicked 'votes for the banana slicer.');
+  // document.write(bathroom.clicked 'votes for the IPAD and toiler paper holder.');
+  // document.write(boots.clicked 'votes for the yellow rainboots.');
+  // document.write(breakfast.clicked 'votes for the all-in-one breakfast maker.');
+  // document.write(bubblegum.clicked 'votes for meatball bubblegum.');
+  // document.write(chair.clicked 'votes for the uncomfortable-looking chair.');
+  // document.write(cthulhu.clicked 'votes for the weird demon-looking creature.');
+  // document.write(dogDuck.clicked 'votes for the dog wearing a duck bill.');
+  // document.write(dragon.clicked 'votes for the canned dragon meat.');
+  // document.write(pen.clicked 'votes for the pen-utensils.');
+  // document.write(petSweep.clicked 'votes for the dust-grabbing slippers for pets.');
+  // document.write(scissors.clicked 'votes for the pizza cutting scissors.');
+  // document.write(shark.clicked 'votes for the shark sleeping bag.');
+  // document.write(sweep.clicked 'votes for the baby outfit that can sweep your floor.');
+  // document.write(tauntaun.clicked 'votes for the Star Wars-themed baby sleeping bag.');
+  // document.write(unicorn.clicked 'votes for canned unicorn meat.');
+  // document.write(usb.clicked 'votes for the wiggling usb.');
+  // document.write(waterCan.clicked 'votes for the self-watering pot.')
+  // document.write(wineGlass.clicked 'votes for the wine glass.');
+// };
+//
+// displayChoices();
 //no need for a for loop, instead do answer++
-
-
-//then write a function that calculates percentages of item chosen when shown
